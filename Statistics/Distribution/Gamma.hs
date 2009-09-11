12f251c455ae48b2c89538dc4af4e5f722e54992
Bryan O'Sullivan <bos@serpentine.com>
2009-09-11 06:39:43 +0000

    Add most of the gamma distribution.

---
+++ Statistics/Distribution/Gamma.hs
@@ -0,0 +1,66 @@
+{-# LANGUAGE DeriveDataTypeable #-}
+-- |
+-- Module    : Statistics.Distribution.Gamma
+-- Copyright : (c) 2009 Bryan O'Sullivan
+-- License   : BSD3
+--
+-- Maintainer  : bos@serpentine.com
+-- Stability   : experimental
+-- Portability : portable
+--
+-- The gamma distribution.  This is a continuous probability
+-- distribution with two parameters, /k/ and &#977;. If /k/ is
+-- integral, the distribution represents the sum of /k/ independent
+-- exponentially distributed random variables, each of which has a
+-- mean of &#977;.
+
+module Statistics.Distribution.Gamma
+    (
+      GammaDistribution
+    -- * Constructors
+    --, fromParams
+    --, fromSample
+    --, standard
+    -- * Accessors
+    , gdShape
+    , gdScale
+    ) where
+
+import Data.Typeable (Typeable)
+import Statistics.Constants (m_huge)
+import Statistics.Math (logGamma)
+import qualified Statistics.Distribution as D
+
+-- | The gamma distribution.
+data GammaDistribution = GD {
+      gdShape :: {-# UNPACK #-} !Double -- ^ Shape parameter, /k/.
+    , gdScale :: {-# UNPACK #-} !Double -- ^ Scale parameter, &#977;.
+    } deriving (Eq, Read, Show, Typeable)
+
+instance D.Distribution GammaDistribution where
+    probability = probability
+    cumulative  = cumulative
+    inverse     = inverse
+
+instance D.Variance GammaDistribution where
+    variance (GD a l) = a / (l * l)
+    {-# INLINE variance #-}
+
+instance D.Mean GammaDistribution where
+    mean (GD a l) = a / l
+    {-# INLINE mean #-}
+
+probability :: GammaDistribution -> Double -> Double
+probability (GD a l) x = x ** (a-1) * exp (-x/l) / (exp (logGamma a) * l ** a)
+{-# INLINE probability #-}
+
+cumulative :: GammaDistribution -> Double -> Double
+cumulative _d _x = undefined
+{-# INLINE cumulative #-}
+
+inverse :: GammaDistribution -> Double -> Double
+inverse d p
+  | p == 0    = -1/0
+  | p == 1    = 1/0
+  | otherwise = D.findRoot d p (gdShape d) 0 m_huge
+{-# INLINE inverse #-}
