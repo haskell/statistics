be7440383bec438a78641ddb35cf57efd4e06357
Bryan O'Sullivan <bos@serpentine.com>
2009-09-18 18:35:37 +0000

    Improve documentation.

--- tests/T.hs
+++ tests/T.hs
@@ -18,29 +18,35 @@ time :: IO a -> IO (Double, a)
 time act = do
   s <- getTime
   ret <- act
-  e <- getTime
+  e <- ret `seq` getTime
   return (e-s, ret)
 
-count = 50000000
+count = floor 1e8
 
-type T = Word64
+type T = Word32
 
 --summ :: [T] -> T
 --summ = foldl' (+) 0
 
-loop :: Monad m => T -> m T -> m T
+loop :: Monad m => Int -> m T -> m T
 loop n act = go 0 0
   where
     go !i !s | i >= n = return s
              | otherwise = do
       d <- act
-      go (i+1) (s+fromIntegral d)
+      go (i+1) (s+d)
 
 mwc :: Word32 -> IO T
 mwc k = return $! runST go where
   go = do
     gen <- initialize (singletonU k)
     loop count (uniform gen)
+
+mwca :: Word32 -> IO T
+mwca k = return $! runST go where
+  go = do
+    gen <- initialize (singletonU k)
+    sumU `fmap` uniformArray gen count
 
 mersenne :: IO T
 mersenne = do
@@ -51,5 +57,7 @@ main = do
   forM_ [1..3] $ \n -> do
     putStr "mwc: "
     print =<< time (mwc n)
+    putStr "mwca: "
+    print =<< time (mwca n)
     putStr "mersenne: "
     print =<< time mersenne
