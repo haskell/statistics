cc9dc749e26198b1315bc6d513a5b2155b1d75be
Khudyakov Alexey <alexey.skladnoy@gmail.com>
2010-07-06 16:53:39 +0000

    Add QC tests for distributions Tests that c.d.f doesn't decrease
    --HG--
    extra : convert_revision : a7eb835e63df94b73ac51638d9aa396140c26823

--- tests/QC.hs
+++ tests/QC.hs
@@ -1,9 +1,27 @@
+import Control.Applicative
 import qualified Data.Vector.Unboxed as U
-import Statistics.Math
-
 import qualified Test.QuickCheck as QC
 
--- Approximate equality (1e-6 is arbitrary)
+import Statistics.Math
+import Statistics.Distribution
+import Statistics.Distribution.Binomial
+import Statistics.Distribution.Exponential
+import Statistics.Distribution.Gamma
+import Statistics.Distribution.Geometric
+import Statistics.Distribution.Hypergeometric
+import Statistics.Distribution.Normal
+import Statistics.Distribution.Poisson
+
+import qualified Statistics.Distribution.Exponential    as Exp
+import qualified Statistics.Distribution.Hypergeometric as Hyper
+import qualified Statistics.Distribution.Normal         as Normal
+import qualified Statistics.Distribution.Poisson        as Poisson
+
+----------------------------------------------------------------
+-- Helpers
+----------------------------------------------------------------
+
+-- | Approximate equality (1e-6 is arbitrary)
 eq :: Double -> Double -> Bool
 eq x y = abs (x - y) < 1e-6
 
@@ -14,8 +32,10 @@ runTests :: [(String, IO ())] -> IO ()
 runTests = mapM_ $ \(name, test) -> putStrLn (" * " ++ name) >> test
 
 ----------------------------------------------------------------
+-- Chebyshev 
+----------------------------------------------------------------
 
--- Chebyshev polynomials
+-- | Chebyshev polynomials
 ch0,ch1,ch2,ch3,ch4 :: Double -> Double
 ch0 _ = 1
 ch1 x = x
@@ -23,10 +43,10 @@ ch2 x = 2*x^2 - 1
 ch3 x = 4*x^3 - 3*x
 ch4 x = 8*x^4 - 8*x^2 + 1
 
--- Test correctness of S.Math.chebysev
+-- | Test correctness of S.Math.chebysev
 testChebyshev :: [(String,IO ())]
 testChebyshev =
-    [ ("Checbyshev polynomials", return ())
+    [ ("==== Checbyshev polynomials ====", return ())
     , ("Deg. 0", p (\(a0,y) ->
                      let x = frac y
                      in eq (ch0 x * a0) (chebyshev x $ U.fromList [a0])))
@@ -46,3 +66,56 @@ testChebyshev =
                            (chebyshev x $ U.fromList [a0,a1,a2,a3,a4])))
     ]
     where frac x = (x - fromIntegral (floor x)) * 2 - 1
+
+----------------------------------------------------------------
+-- Tests for probabilites distributions
+----------------------------------------------------------------
+          
+-- Arbitrary instances for ditributions
+instance QC.Arbitrary BinomialDistribution where
+  arbitrary = binomial <$> QC.choose (1,100) <*> QC.choose (0,1)
+instance QC.Arbitrary ExponentialDistribution where
+  arbitrary = Exp.fromLambda <$> QC.choose (0,100)
+instance QC.Arbitrary GeometricDistribution where
+  arbitrary = fromSuccess <$> QC.choose (0,1)
+instance QC.Arbitrary HypergeometricDistribution where
+  arbitrary = do l <- QC.choose (1,20)
+                 m <- QC.choose (1,l)
+                 k <- QC.choose (1,l)
+                 return $ Hyper.fromParams m l k
+instance QC.Arbitrary NormalDistribution where
+  arbitrary = Normal.fromParams <$> QC.choose (-100,100) <*> QC.choose (1e-3, 1e3)
+instance QC.Arbitrary PoissonDistribution where
+  arbitrary = Poisson.fromLambda <$> QC.choose (0,1)
+
+-- CDF must be non-decreasing
+type CDFMonotonityCheck d = (d,Double,Double) -> Bool
+
+cdfMonotonityCheck :: (Distribution d, QC.Arbitrary d) => CDFMonotonityCheck d
+cdfMonotonityCheck (d,x1,x2) = 
+  cumulative d (min x1 x2) <= cumulative d (max x1 x2)
+
+
+-- | Tests for distributions
+testDistr :: [(String, IO ())]
+testDistr = 
+  [ ("==== CDF monotonity checks ====", return ())
+  , ("Binomial",       p (cdfMonotonityCheck :: CDFMonotonityCheck BinomialDistribution))
+  , ("Exponential",    p (cdfMonotonityCheck :: CDFMonotonityCheck ExponentialDistribution))
+  -- , ("Gamma",          cdfMonotonityCheck :: CDFMonotonityCheck Gamma)
+  , ("Geometric",      p (cdfMonotonityCheck :: CDFMonotonityCheck GeometricDistribution))
+  , ("Hypergeometric", p (cdfMonotonityCheck :: CDFMonotonityCheck HypergeometricDistribution))
+  , ("Normal",         p (cdfMonotonityCheck :: CDFMonotonityCheck NormalDistribution))
+  , ("Poisson",        p (cdfMonotonityCheck :: CDFMonotonityCheck PoissonDistribution))
+  ]
+
+
+----------------------------------------------------------------
+-- 
+----------------------------------------------------------------
+
+-- | Complete list of tests
+testAll :: [(String, IO())]
+testAll = concat [ testChebyshev
+                 , testDistr
+                 ]
