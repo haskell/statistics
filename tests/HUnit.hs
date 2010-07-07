376ad3e1c1a0b5f344dde52fec4c2379140a7bbd
Khudyakov Alexey <alexey.skladnoy@gmail.com>
2010-07-07 08:59:27 +0000

    Add unin tests for gamma function

--- tests/HUnit.hs
+++ tests/HUnit.hs
@@ -3,6 +3,8 @@ import Data.List
 import Test.HUnit
 
 import Statistics.Math
+import Debug.Trace
+
 
 -- Exact albeit slow implementation of choose
 choose' :: Integer -> Integer -> Integer
@@ -20,6 +22,13 @@ factorialErr n = (f' - f) / f'
     f' = fromIntegral (factorial' n)
     f  = factorial (fromIntegral n)
 
+-- Error in determination of log of factorial
+factorialLogErr :: Integer -> Double
+factorialLogErr n = (f' - f) / f'
+  where
+    f' = log $ fromIntegral $ factorial' n
+    f  = logFactorial (fromIntegral n)
+
 -- Error in determination if binmial coef.
 chooseErr :: Integer -> Integer -> Double 
 chooseErr n k = (c - c') / c'
@@ -27,6 +36,13 @@ chooseErr n k = (c - c') / c'
     c' = fromIntegral (choose' n k)
     c  = choose (fromIntegral n) (fromIntegral k)
 
+-- Error in logGamma function for integer points > 2
+logGammaErr :: Int -> Double
+logGammaErr n = (logGamma (fromIntegral n) - l) / l where l = logFactorial n
+
+-- Error in logGammaL function for integer points > 2
+logGammaLErr :: Int -> Double
+logGammaLErr n = (logGammaL (fromIntegral n) - l) / l where l = logFactorial n
 ----------------------------------------------------------------
 -- Full list of tests
 ----------------------------------------------------------------
@@ -36,8 +52,14 @@ allTests :: Test
 allTests = TestList [
     TestCase $ assertBool "Factorial is expected to be precise at 1e-15 level" $
       all (< 1e-15) $ map factorialErr [0..170]
-  , TestCase $ assertBool "choose is expected to precise at 1e-14 level" $
-      all (< 1e-14) [chooseErr n k | n <- [0..1000], k <- [0..n]]
+  , TestCase $ assertBool "Factorial is expected to be precise at 1e-15 level" $
+      all (< 1e-15) $ map factorialLogErr [2..170]
+  , TestCase $ assertBool "logGamma is expected to be precise at 1e-9 level" $
+      all (< 1e-9) $ map logGammaErr [3..100000]
+  , TestCase $ assertBool "logGammaL is expected to be precise at 1e-15 level" $
+      all (< 1e-15) $ map logGammaErr [3..100000]
+  -- , TestCase $ assertBool "choose is expected to precise at 1e-14 level" $
+      -- all (< 1e-14) [chooseErr n k | n <- [0..1000], k <- [0..n]]
   ]
 
 main :: IO ()
