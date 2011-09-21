6e298e98e65667191f6189628993eaa949a947ca
Alexey Khudyakov <alexey.skladnoy@gmail.com>
2011-09-22 01:43:01 +0400

    Migrate tests for nonparametric tests to test framework

--- tests/tests.hs
+++ tests/tests.hs
@@ -2,8 +2,11 @@ import Test.Framework       (defaultMain)
 
 import Tests.Distribution
 import Tests.Math
+import Tests.NonparametricTest
+
 
 main :: IO ()
 main = defaultMain [ distributionTests 
                    , mathTests
+                   , nonparametricTests
                    ]
