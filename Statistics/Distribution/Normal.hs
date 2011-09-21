81a3167c2e744de1bc8fc24eca71d282c9f2f187
Alexey Khudyakov <alexey.skladnoy@gmail.com>
2011-09-21 23:01:52 +0400

    Pass standard deviation to constructor of normal distribution instead of variance
    Breaks API!

--- Statistics/Distribution/Normal.hs
+++ Statistics/Distribution/Normal.hs
@@ -29,7 +29,7 @@ import qualified Statistics.Sample as S
 -- | The normal distribution.
 data NormalDistribution = ND {
       mean       :: {-# UNPACK #-} !Double
-    , variance   :: {-# UNPACK #-} !Double
+    , stdDev     :: {-# UNPACK #-} !Double
     , ndPdfDenom :: {-# UNPACK #-} !Double
     , ndCdfDenom :: {-# UNPACK #-} !Double
     } deriving (Eq, Read, Show, Typeable)
@@ -42,7 +42,7 @@ instance D.ContDistr NormalDistribution where
     quantile   = quantile
 
 instance D.Variance NormalDistribution where
-    variance = variance
+    stdDev = stdDev
 
 instance D.Mean NormalDistribution where
     mean = mean
@@ -50,34 +50,37 @@ instance D.Mean NormalDistribution where
 -- | Standard normal distribution with mean equal to 0 and variance equal to 1
 standard :: NormalDistribution
 standard = ND { mean       = 0.0
-              , variance   = 1.0
+              , stdDev     = 1.0
               , ndPdfDenom = m_sqrt_2_pi
               , ndCdfDenom = m_sqrt_2
               }
 
--- | Create normal distribution from parameters
+-- | Create normal distribution from parameters.
+--
+-- IMPORTANT: prior to 0.10 release second parameter was variance not
+-- standard deviation.
 normalDistr :: Double            -- ^ Mean of distribution
-            -> Double            -- ^ Variance of distribution
+            -> Double            -- ^ Standard deviation of distribution
             -> NormalDistribution
-normalDistr m v
-  | v <= 0    = 
-    error $ "Statistics.Distribution.Normal.normalDistr: variance must be positive. Got " ++ show v
-  | otherwise = ND { mean       = m
-                   , variance   = v
-                   , ndPdfDenom = m_sqrt_2_pi * sv
-                   , ndCdfDenom = m_sqrt_2 * sv
+normalDistr m sd
+  | sd > 0    = ND { mean       = m
+                   , stdDev     = sd
+                   , ndPdfDenom = m_sqrt_2_pi * sd
+                   , ndCdfDenom = m_sqrt_2 * sd
                    }
-    where sv = sqrt v
+  | otherwise = 
+    error $ "Statistics.Distribution.Normal.normalDistr: standard deviation must be positive. Got " ++ show sd
 
 -- | Create distribution using parameters estimated from
 --   sample. Variance is estimated using maximum likelihood method
 --   (biased estimation).
 normalFromSample :: S.Sample -> NormalDistribution
-normalFromSample a = normalDistr (S.mean a) (S.variance a)
+normalFromSample a = normalDistr (S.mean a) (S.stdDev a)
 
 density :: NormalDistribution -> Double -> Double
-density d x = exp (-xm * xm / (2 * variance d)) / ndPdfDenom d
+density d x = exp (-xm * xm / (2 * sd * sd)) / ndPdfDenom d
     where xm = x - mean d
+          sd = stdDev d
 
 cumulative :: NormalDistribution -> Double -> Double
 cumulative d x = erfc ((mean d - x) / ndCdfDenom d) / 2
@@ -88,6 +91,6 @@ quantile d p
   | p == 0         = -inf
   | p == 1         = inf
   | p == 0.5       = mean d
-  | otherwise      = x * sqrt (variance d) + mean d
+  | otherwise      = x * stdDev d + mean d
   where x          = D.findRoot standard p 0 (-100) 100
         inf        = 1/0
