6136b778a04d70b365d4785ade0e1e6cda56f794
Alexey Khudyakov <alexey.skladnoy@gmail.com>
2011-09-24 20:10:39 +0400

    Take into account rounding errors in tests for CDF monotonity
    Fixes spurious failure in Poisson test
    
    Dependency on ieee754 for test suite

--- tests/Tests/Helpers.hs
+++ tests/Tests/Helpers.hs
@@ -7,11 +7,14 @@ module Tests.Helpers (
   , (=~)
     -- * Generic QC tests
   , monotonicallyIncreases
+  , monotonicallyIncreasesIEEE
     -- * HUnit helpers
   , testAssertion
   ) where
 
 import Data.Typeable
+
+import qualified Numeric.IEEE    as IEEE
 
 import qualified Test.HUnit      as HU
 import Test.Framework
@@ -49,11 +52,23 @@ eq eps a b
 -- Generic QC
 ----------------------------------------------------------------
 
--- Check that function is monotonically increasing
+-- Check that function is nondecreasing
 monotonicallyIncreases :: (Ord a, Ord b) => (a -> b) -> a -> a -> Bool
 monotonicallyIncreases f x1 x2 = f (min x1 x2) <= f (max x1 x2)
 
-
+-- Check that function is nondecreasing taking rounding errors into
+-- account.
+--
+-- In fact funstion is allowed to decrease less than one ulp in order
+-- to guard againist problems with excess precision. On x86 FPU works
+-- with 80-bit numbers but doubles are 64-bit so rounding happens
+-- whenever values are moved from registers to memory
+monotonicallyIncreasesIEEE :: (Ord a, IEEE.IEEE b)  => (a -> b) -> a -> a -> Bool
+monotonicallyIncreasesIEEE f x1 x2 =
+  y1 <= y2 || (y1 - y2) < y2 * IEEE.epsilon
+  where
+    y1 = f (min x1 x2)
+    y2 = f (max x1 x2)
 
 ----------------------------------------------------------------
 -- HUnit helpers
