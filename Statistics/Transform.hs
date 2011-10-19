4ff5ddc61947e057436f6518e7a3f2a2d08291fc
Bryan O'Sullivan <bos@serpentine.com>
2011-10-19 01:10:57 -0700

    Apparently broken in-place FFT :-(

--- Statistics/Transform.hs
+++ Statistics/Transform.hs
@@ -22,11 +22,17 @@ module Statistics.Transform
     , ifft
     ) where
 
+import Debug.Trace
 import Control.Arrow ((***))
-import Data.Bits (shiftR)
+import Control.Monad (when)
+import Control.Monad.ST (ST)
+import Data.Bits (shiftL, shiftR)
 import Data.Complex (Complex(..), realPart)
+import Statistics.Math (log2)
 import qualified Data.Vector as V
 import qualified Data.Vector.Generic as G
+import qualified Data.Vector.Generic.Mutable as M
+import qualified Data.Vector.Unboxed as U
 
 type CD = Complex Double
 
@@ -56,15 +62,17 @@ idct xs = G.generate len interleave
 ifft :: V.Vector CD -> V.Vector CD
 ifft xs = G.map (/fi (G.length xs)) (fft xs)
 
--- | Fast Fourier transform.
-fft :: V.Vector CD -> V.Vector CD
-fft xs = case G.length xs of
+fft v = let x = fft2 v
+        in traceShow (v,x) x
+
+fft1 :: V.Vector CD -> V.Vector CD
+fft1 xs = case G.length xs of
             0 -> G.empty
             2 -> dft xs
             _ -> G.zipWith (flip ($)) (zs G.++ zs) .
                  G.zipWith (flip ($)) (ys G.++ ys) $
                  (G.map f ks G.++ G.map g ks) 
-  where (ys,zs) = (fft***fft) . G.unzip . pairs $ xs
+  where (ys,zs) = (fft1***fft1) . G.unzip . pairs $ xs
         ks = G.enumFromTo 0 ((len*2)-1)
         f k x y = x + y * exp (negate(2*pi*i*fi k)/fi(len*2))
         g k x y = x - y * exp (negate(2*pi*i*fi k)/fi(len*2))
@@ -72,6 +80,44 @@ fft xs = case G.length xs of
                   let z = j * 2
                   in (v `G.unsafeIndex` z, v `G.unsafeIndex` (z+1))
         len = G.length ys
+
+-- | Fast Fourier transform.
+fft2 :: V.Vector CD -> V.Vector CD
+fft2 v = G.create $ do
+           mv <- G.thaw v
+           mfft mv
+           return mv
+
+mfft :: (M.MVector v CD) => v s CD -> ST s ()
+mfft vec = do
+  bitReverse 0 0
+  stage 0 1 (-1) 0
+ where
+  bitReverse i j | i == (len-1) = return ()
+                 | otherwise = do
+    when (i < j) $ M.swap vec i j
+    let inner k l | k <= l    = inner (l-k) (k `shiftR` 1)
+                  | otherwise = bitReverse (i+1) (l+k)
+    inner (len `shiftR` 1) j
+  stage l !l1 !c1 !c2 | l == log2 len = return ()
+                      | otherwise = do
+    let !l2 = l1 `shiftL` 1
+        flight j !u1 !u2 | j == l1 = return ()
+                         | otherwise = do
+          let butterfly i | i >= len = return ()
+                          | otherwise = do
+                let i1 = i + l1
+                ci1@(xi1 :+ yi1) <- M.read vec i1
+                let d = (u1*xi1 - u2*yi1) :+ (u1*yi1 + u2*xi1)
+                ci <- M.read vec i
+                M.write vec i1 (ci - d)
+                M.write vec i (ci + d)
+                butterfly (i+l2)
+          butterfly j
+          flight (j+1) (u1*c1 - u2*c2) (u1*c2 + u2*c1)
+    flight 0 1 0
+    stage (l+1) l2 (sqrt ((1+c1) / 2)) (sqrt ((1-c1) / 2))
+  len = M.length vec
 
 -- | Discrete Fourier transform.
 dft :: V.Vector CD -> V.Vector CD
