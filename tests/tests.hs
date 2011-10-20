2bc4822f1fde413ee18c43c4e8b75426648b87d4
Bryan O'Sullivan <bos@serpentine.com>
2011-10-19 17:44:22 -0700

    Write some simple FFT properties.

--- tests/tests.hs
+++ tests/tests.hs
@@ -3,10 +3,11 @@ import Test.Framework       (defaultMain)
 import Tests.Distribution
 import Tests.Math
 import Tests.NonparametricTest
-
+import qualified Tests.Transform
 
 main :: IO ()
 main = defaultMain [ distributionTests 
                    , mathTests
                    , nonparametricTests
+                   , Tests.Transform.tests
                    ]
