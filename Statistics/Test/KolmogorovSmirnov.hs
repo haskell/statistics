74bcca48f6895331896b1d789829dc184e084ed6
Alexey Khudyakov <alexey.skladnoy@gmail.com>
2011-11-01 19:41:39 +0400

    Implement calculation of D statitistics for 2-sample Kolmogorov-Smirnov test

--- Statistics/Test/KolmogorovSmirnov.hs
+++ Statistics/Test/KolmogorovSmirnov.hs
@@ -32,3 +32,35 @@ kolmogorovSmirnovD :: (Distribution d)
                    -> Double
 kolmogorovSmirnovD d = kolmogorovSmirnovCdfD (cumulative d)
 {-# INLINE kolmogorovSmirnovD #-}
+
+-- | Calculate /D/ statistics for two samples
+kolmogorovSmirnov2D :: Sample   -- ^ First sample
+                    -> Sample   -- ^ Second sample
+                    -> Double
+kolmogorovSmirnov2D sample1 sample2
+  | U.null sample1 || U.null sample2 = 0
+  | otherwise                        = worker 0 0 0
+  where
+    xs1 = sort sample1
+    xs2 = sort sample2
+    n1  = U.length xs1
+    n2  = U.length xs2
+    en1 = fromIntegral n1
+    en2 = fromIntegral n2
+    -- Find new index
+    skip x i xs = go (i+1)
+      where go n | n >= U.length xs = n
+                 | xs U.! n == x    = go (n+1)
+                 | otherwise        = n
+    -- Main loop
+    worker d i1 i2
+      | i1 >= n1 || i2 >= n2 = d
+      | otherwise            = worker d' i1' i2'
+      where
+        d1  = xs1 U.! i1
+        d2  = xs2 U.! i2
+        i1' | d1 <= d2  = skip d1 i1 xs1
+            | otherwise = i1
+        i2' | d2 <= d1  = skip d2 i2 xs2
+            | otherwise = i2
+        d'  = max d (abs $ fromIntegral i1' / en1 - fromIntegral i2' / en2)
