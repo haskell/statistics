6eea1a59f17b08c4d1e984b023c683f8cdb4cfe4
Alexey Khudyakov <alexey.skladnoy@gmail.com>
2012-02-05 02:04:02 +0400

    Update test suite after math-functions split
    Tests for S.Math are now in math-functions
    Update import

--- tests/tests.hs
+++ tests/tests.hs
@@ -1,14 +1,12 @@
 import Test.Framework       (defaultMain)
 
 import Tests.Distribution
-import Tests.Math
 import Tests.NonparametricTest
 import qualified Tests.Transform
 import qualified Tests.Function
 
 main :: IO ()
 main = defaultMain [ distributionTests 
-                   , mathTests
                    , nonparametricTests
                    , Tests.Transform.tests
                    , Tests.Function.tests
