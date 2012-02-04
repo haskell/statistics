72736ca078be1e0054ba3a5e0ed76c112820b671
Alexey Khudyakov <alexey.skladnoy@gmail.com>
2012-02-05 02:02:18 +0400

    Add test for sort

--- tests/tests.hs
+++ tests/tests.hs
@@ -4,10 +4,12 @@ import Tests.Distribution
 import Tests.Math
 import Tests.NonparametricTest
 import qualified Tests.Transform
+import qualified Tests.Function
 
 main :: IO ()
 main = defaultMain [ distributionTests 
                    , mathTests
                    , nonparametricTests
                    , Tests.Transform.tests
+                   , Tests.Function.tests
                    ]
