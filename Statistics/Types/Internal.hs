ecfcc0ac6cd25ffa750b239dd70aeee1e00c8eec
Alexey Khudyakov <alexey.skladnoy@gmail.com>
2014-06-06 21:33:25 +0400

    Add data type for confidence level
    It require creation of internal module to break dependency cycle

---
+++ Statistics/Types/Internal.hs
@@ -0,0 +1,35 @@
+-- |
+-- Module    : Statistics.Types.Internal
+-- Copyright : (c) 2009 Bryan O'Sullivan
+-- License   : BSD3
+--
+-- Maintainer  : bos@serpentine.com
+-- Stability   : experimental
+-- Portability : portable
+--
+-- Types for working with statistics.
+module Statistics.Types.Internal where
+
+
+import qualified Data.Vector.Unboxed as U (Vector)
+
+-- | Sample data.
+type Sample = U.Vector Double
+
+-- | Sample with weights. First element of sample is data, second is weight
+type WeightedSample = U.Vector (Double,Double)
+
+-- | An estimator of a property of a sample, such as its 'mean'.
+--
+-- The use of an algebraic data type here allows functions such as
+-- 'jackknife' and 'bootstrapBCA' to use more efficient algorithms
+-- when possible.
+data Estimator = Mean
+               | Variance
+               | VarianceUnbiased
+               | StdDev
+               | Function (Sample -> Double)
+
+-- | Weights for affecting the importance of elements of a sample.
+type Weights = U.Vector Double
+
