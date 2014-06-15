64003ec7ab0300698218764e776c07d8473b16d2
Alexey Khudyakov <alexey.skladnoy@gmail.com>
2014-06-15 22:59:34 +0400

    Add types for statistical test results

--- Statistics/Test/Types.hs
+++ Statistics/Test/Types.hs
@@ -1,12 +1,32 @@
-{-# LANGUAGE DeriveDataTypeable #-}
+{-# LANGUAGE DeriveFunctor, DeriveDataTypeable #-}
 module Statistics.Test.Types (
-    TestType(..)
+    Test(..)
+  , isSignificant
   , TestResult(..)
   , significant
+  , TestType(..)
   ) where
 
 import Data.Typeable (Typeable)
+import Statistics.Types (CL)
 
+-- | Result of hypothesis testing
+data TestResult = Significant    -- ^ Null hypothesis should be rejected
+                | NotSignificant -- ^ Data is compatible with hypothesis
+                  deriving (Eq,Ord,Show,Typeable)
+
+-- | Result of statistical test. It contains test p-value (probability of encountering
+data Test a = Test
+  { testSignificance :: CL Double
+  , testExtraData    :: a
+  }
+  deriving (Eq,Ord,Show,Typeable,Functor)
+
+-- | Check whether test is significant for given p-value.
+isSignificant :: CL Double -> Test a -> TestResult
+isSignificant cl t
+  -- FIXME: check what Ord instance have correct meaning
+  = significant $ cl <= testSignificance t
 
 -- | Test type. Exact meaning depends on a specific test. But
 -- generally it's tested whether some statistics is too big (small)
@@ -14,11 +34,6 @@ import Data.Typeable (Typeable)
 data TestType = OneTailed
               | TwoTailed
               deriving (Eq,Ord,Show,Typeable)
-
--- | Result of hypothesis testing
-data TestResult = Significant    -- ^ Null hypothesis should be rejected
-                | NotSignificant -- ^ Data is compatible with hypothesis
-                  deriving (Eq,Ord,Show,Typeable)
 
 -- | Significant if parameter is 'True', not significant otherwiser
 significant :: Bool -> TestResult
