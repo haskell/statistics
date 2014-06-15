4dcf7f7e0526d3d375f86da022b268730b5ae995
Alexey Khudyakov <alexey.skladnoy@gmail.com>
2014-06-15 23:11:54 +0400

    Convert Chi2 test to new API

--- Statistics/Test/ChiSquared.hs
+++ Statistics/Test/ChiSquared.hs
@@ -8,10 +8,13 @@ module Statistics.Test.ChiSquared (
   ) where
 
 import Prelude hiding (sum)
+import Control.Monad
+
 import Statistics.Distribution
 import Statistics.Distribution.ChiSquared
 import Statistics.Sample.Internal (sum)
 import Statistics.Test.Types
+import Statistics.Types
 import qualified Data.Vector.Generic as G
 
 
@@ -19,22 +22,31 @@ import qualified Data.Vector.Generic as G
 --   sample is supplied in form of tuples (observed quantity,
 --   expected number of events). Both must be positive.
 chi2test :: (G.Vector v (Int,Double), G.Vector v Double)
-         => Double              -- ^ p-value
-         -> Int                 -- ^ Number of additional degrees of
+         => Int                 -- ^ Number of additional degrees of
                                 --   freedom. One degree of freedom
                                 --   is due to the fact that the are
                                 --   N observation in total and
                                 --   accounted for automatically.
          -> v (Int,Double)      -- ^ Observation and expectation.
-         -> TestResult
-chi2test p ndf vec
-  | ndf < 0        = error $ "Statistics.Test.ChiSquare.chi2test: negative NDF " ++ show ndf
-  | n   < 0        = error $ "Statistics.Test.ChiSquare.chi2test: too short data sample"
-  | p > 0 && p < 1 = significant $ complCumulative d chi2 < p
-  | otherwise      = error $ "Statistics.Test.ChiSquare.chi2test: bad p-value: " ++ show p
+         -> Maybe (Test Chi2Data)
+chi2test ndf vec
+  | ndf <  0  = error $ "Statistics.Test.ChiSquare.chi2test: negative NDF " ++ show ndf
+  | n   <= 0  = error $ "Statistics.Test.ChiSquare.chi2test: too short data sample"
+  | n > 0     = Just $ Test
+              { testSignificance = CL (complCumulative d chi2)
+              , testExtraData    = Chi2Data n chi2
+              }
+  | otherwise = Nothing
   where
     n     = G.length vec - ndf - 1
     chi2  = sum $ G.map (\(o,e) -> sqr (fromIntegral o - e) / e) vec
     d     = chiSquared n
     sqr x = x * x
 {-# INLINE chi2test #-}
+
+
+-- | Extra data returned by chi-square test
+data Chi2Data = Chi2Data
+  { chi2DataNDF  :: !Int         -- ^ Number of degrees of freedom
+  , chi2DataChi2 :: !Double      -- ^ Calculated χ²
+  }
