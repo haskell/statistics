b477b7c5c95f1a9a6a83dca9b0e5b8ba9219408b
Kai Zhang <kai@kzhang.org>
2014-11-19 16:53:17 -0800

    name change

--- Statistics/Correlation/Spearman.hs
+++ Statistics/Correlation/Spearman.hs
@@ -7,7 +7,7 @@
 module Statistics.Correlation.Spearman
     ( spearman
     , spearmanMatByRow
-    , ranking
+    , computeRank
     ) where
 
 import Data.Ord
@@ -35,33 +35,32 @@ spearman :: ( Ord a
          -> Double
 spearman xy = pearson xy'
   where
-    xy' = G.zip (ranking x) $ ranking y    
+    xy' = G.zip (computeRank x) $ computeRank y    
     (x, y) = G.unzip xy
 {-# INLINE spearman #-}
 
 -- | compute pairwise spearman correlation between rows of a matrix
 spearmanMatByRow :: Matrix -> Matrix
-spearmanMatByRow = pearsonMatByRow . fromRows . map ranking . toRows
+spearmanMatByRow = pearsonMatByRow . fromRows . map computeRank . toRows
 {-# INLINE spearmanMatByRow #-}
 
--- Note: for sorted data, use Statistics.Test.Internal.rank
--- | compute ranks for unsorted data
-ranking :: ( Ord a
-           , G.Vector v a
-           , G.Vector v Int
-           , G.Vector v Double
-           , G.Vector v (Int, a)
-           , G.Vector v (Int, Double)
-           )
-        => v a
-        -> v Double
-ranking xs = G.create $ do
-    vec <- GM.new n
-    G.mapM_ (f vec) . G.zip index $ ranks
-    return vec
+-- Note: for sorted vector, use Statistics.Test.Internal.rank
+-- | compute rank of an unsorted vector
+computeRank :: ( Ord a
+               , G.Vector v a
+               , G.Vector v Int
+               , G.Vector v Double
+               , G.Vector v (Int, a)
+               , G.Vector v (Int, Double)
+               )
+            => v a
+            -> v Double
+computeRank xs = G.create $ do vec <- GM.new n
+                               G.mapM_ (f vec) . G.zip index $ ranks
+                               return vec
   where
     (index, xs') = G.unzip . sortBy (comparing snd) . G.zip (G.enumFromN 0 n) $ xs
     ranks = rank (==) xs'
     f v (idx, val) = GM.unsafeWrite v idx val
     n = G.length xs
-{-# INLINE ranking #-}
+{-# INLINE computeRank #-}
