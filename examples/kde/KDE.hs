1cab211a2858cadb0f40ccefd33c2b1804aa3917
Björn Döring <bjoern.doering@gmail.com>
2014-12-30 09:56:06 +0100

    Update example KDE.hs to newer hastache

--- examples/kde/KDE.hs
+++ examples/kde/KDE.hs
@@ -4,11 +4,12 @@ import Control.Applicative ((<$>))
 import Statistics.Sample.KernelDensity (kde)
 import Text.Hastache (MuType(..), defaultConfig, hastacheFile)
 import Text.Hastache.Context (mkStrContext)
-import qualified Data.Attoparsec as B
-import qualified Data.Attoparsec.Char8 as A
+import qualified Data.Attoparsec.ByteString as B
+import qualified Data.Attoparsec.ByteString.Char8 as A
 import qualified Data.ByteString as B
 import qualified Data.ByteString.Lazy as L
 import qualified Data.Vector.Unboxed as U
+import qualified Data.Text.Lazy.IO as TL
 
 csv = do
   B.takeTill A.isEndOfLine
@@ -20,4 +21,4 @@ main = do
   let xs = map (\(a,b) -> [a,b]) . U.toList . uncurry U.zip . kde 64 $ waits
       context "data" = MuVariable . show $ xs
   s <- hastacheFile defaultConfig "kde.tpl" (mkStrContext context)
-  L.writeFile "kde.html" s
+  TL.writeFile "kde.html" s
