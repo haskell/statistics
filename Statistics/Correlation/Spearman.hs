b1405f1495baa5bc7ed28b5117b710329194ff5a
Alexey Khudyakov <alexey.skladnoy@gmail.com>
2015-01-21 00:20:53 +0300

    Refactor rank for unsorted vector

--- Statistics/Correlation/Spearman.hs
+++ Statistics/Correlation/Spearman.hs
@@ -1,22 +1,17 @@
 {-# LANGUAGE FlexibleContexts #-}
-
 -- |
 -- Module      : Statistics.Correlation.Spearman
 --
-
 module Statistics.Correlation.Spearman
     ( spearman
     , spearmanMatByRow
-    , computeRank
     ) where
 
-import Data.Ord
 import qualified Data.Vector.Generic as G
-import qualified Data.Vector.Generic.Mutable as GM
 import Statistics.Correlation.Pearson
-import Statistics.Function
 import Statistics.Matrix hiding (map)
-import Statistics.Test.Internal (rank)
+import Statistics.Test.Internal (rankUnsorted)
+
 
 -- | compute spearman correlation between two samples
 spearman :: ( Ord a
@@ -35,32 +30,11 @@ spearman :: ( Ord a
          -> Double
 spearman xy = pearson xy'
   where
-    xy' = G.zip (computeRank x) $ computeRank y    
+    xy' = G.zip (rankUnsorted x) $ rankUnsorted y
     (x, y) = G.unzip xy
 {-# INLINE spearman #-}
 
 -- | compute pairwise spearman correlation between rows of a matrix
 spearmanMatByRow :: Matrix -> Matrix
-spearmanMatByRow = pearsonMatByRow . fromRows . map computeRank . toRows
+spearmanMatByRow = pearsonMatByRow . fromRows . map rankUnsorted . toRows
 {-# INLINE spearmanMatByRow #-}
-
--- Note: for sorted vector, use Statistics.Test.Internal.rank
--- | compute rank of an unsorted vector
-computeRank :: ( Ord a
-               , G.Vector v a
-               , G.Vector v Int
-               , G.Vector v Double
-               , G.Vector v (Int, a)
-               , G.Vector v (Int, Double)
-               )
-            => v a
-            -> v Double
-computeRank xs = G.create $ do vec <- GM.new n
-                               G.mapM_ (f vec) . G.zip index $ ranks
-                               return vec
-  where
-    (index, xs') = G.unzip . sortBy (comparing snd) . G.zip (G.enumFromN 0 n) $ xs
-    ranks = rank (==) xs'
-    f v (idx, val) = GM.unsafeWrite v idx val
-    n = G.length xs
-{-# INLINE computeRank #-}
