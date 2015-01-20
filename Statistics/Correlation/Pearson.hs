a90ed9e043520124d814319e287403b8bd51b29f
Alexey Khudyakov <alexey.skladnoy@gmail.com>
2015-01-21 00:25:19 +0300

    Use correlation from S.Sample

--- Statistics/Correlation/Pearson.hs
+++ Statistics/Correlation/Pearson.hs
@@ -7,9 +7,6 @@
 module Statistics.Correlation.Pearson
     ( pearson
     , pearsonMatByRow
-
-    -- * References
-    -- $references
     ) where
 
 import qualified Data.Vector.Generic as G
@@ -18,26 +15,10 @@ import qualified Data.Vector.Unboxed.Mutable as UM
 import Statistics.Matrix
 import Statistics.Sample
 
--- | use Welford's one-pass algorithm to calculate the Pearson's correlation 
--- between two samples.
-pearson :: G.Vector v (Double, Double) => v (Double, Double) -> Double
-pearson xy | n <= 1 = 0/0
-           | otherwise = sum_cross / (sqrt sum_xsq * sqrt sum_ysq)
-  where
-    (sum_cross, sum_xsq, sum_ysq, _, _, _) = G.foldl step (0,0,0,0,0,0::Int) xy
-    step (!vxy, !vx, !vy, !mx, !my, !i) (x, y) =
-        ( vxy + delta_x * delta_y * ratio
-        , vx + delta_x * delta_x * ratio
-        , vy + delta_y * delta_y * ratio
-        , mx + delta_x / fromIntegral (i+1)
-        , my + delta_y / fromIntegral (i+1)
-        , i + 1
-        )
-      where
-        delta_x = x - mx
-        delta_y = y - my
-        ratio = fromIntegral i / fromIntegral (i+1)
-    n = G.length xy
+-- | Pearson correlation for sample of pairs.
+pearson :: (G.Vector v (Double, Double), G.Vector v Double)
+        => v (Double, Double) -> Double
+pearson = correlation
 {-# INLINE pearson #-}
 
 -- | compute pairwise pearson correlation between rows of a matrix
@@ -60,27 +41,3 @@ pearsonMatByRow m = fromVector n n $ U.create $ UM.replicate (n*n) 1.0 >>= loopR
         vx = m `row` i
     n = rows m
 {-# INLINE pearsonMatByRow #-}
-
--- calculate pearson correlation by definition, just for testing purpose
-pearson' :: (G.Vector v Double, G.Vector v (Double, Double)) => v (Double, Double) -> Double
-pearson' xy | n <= 1 = 0/0
-            | otherwise = cov / sqrt (var_x * var_y)
-  where
-    (x, y) = G.unzip xy
-    m_x = mean x
-    m_y = mean y
-    (cov, var_x, var_y) = loop (0, 0, 0) 0
-    loop (!co, !vx, !vy) i | i >= n = (co, vx, vy)
-                           | otherwise = let t1 = x G.! i - m_x
-                                             t2 = y G.! i - m_y
-                                         in loop ( co + t1 * t2
-                                                 , vx + t1 * t1
-                                                 , vy + t2 * t2
-                                                 ) (i+1)
-    n = G.length x
-{-# INLINE pearson' #-}
-
--- $references
---
--- * B. P. Welford. (1962) Note on a Method for Calculating Corrected Sums of 
--- Squares and Products. /Technometrics/, Vol 4, No 3, 1962.
