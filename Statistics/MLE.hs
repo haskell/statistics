e98af025ef4aa0bc31a5b1fcf88bb80295aac956
Stefan Hubner <s.hubner@tilburguniversity.edu>
2015-03-13 13:55:45 +0100

    MLE probit, cumulative range bug circumvented

--- Statistics/MLE.hs
+++ Statistics/MLE.hs
@@ -7,38 +7,33 @@ import Numeric.GSL.Minimization
 import Statistics.Distribution
 import Statistics.Distribution.Normal
 import Statistics.Matrix
-import System.Random.MWC
-import Control.Applicative
-import Control.Monad
 import qualified Data.Vector.Generic as G
 import qualified Data.Vector.Unboxed as U
 
 data MaxControl = MaxControl { maxit :: Int, epsilon :: Double } deriving (Show)
 
-type ObjectiveF = Matrix -> Vector -> [Double] -> Double
-type GradientF = Matrix -> Vector -> [Double] -> [Double] 
+type ObjectiveF = [Double] -> Double
+type GradientF = [Double] -> [Double]
+type LikelihoodModel = Matrix -> Vector -> (ObjectiveF, GradientF)
 
 defaultMaxCtrl :: MaxControl
-defaultMaxCtrl = MaxControl 10 10e-2
+defaultMaxCtrl = MaxControl 100 10e-2
 
-maxLikelihood :: ObjectiveF -> GradientF -> MaxControl -> Matrix -> Vector -> Vector 
-maxLikelihood obj dobj ctrl x y = U.fromList . fst $ bhat 
+maxLikelihood :: LikelihoodModel -> MaxControl -> Matrix -> Vector -> Vector 
+maxLikelihood model ctrl x y = U.fromList . fst $ bhat 
 	where 
-		bhat = minimizeD VectorBFGS2 (epsilon ctrl) (maxit ctrl) 1.0 (epsilon ctrl) (obj x y) (dobj x y) sval
-		sval = replicate (snd (dimension x)) 0.0 
+		bhat = uncurry (minimizeD VectorBFGS2 (epsilon ctrl) (maxit ctrl) 1.0 0.1) (model x y) sval
+		sval = replicate (snd (dimension x)) (epsilon ctrl) 
 
-llprobit :: ObjectiveF
-llprobit x y b = negate $ G.sum $ G.map contr (G.zip y (x `multiplyV` U.fromList b))
+llprobit :: LikelihoodModel 
+llprobit x y = (loglik, grad)  
 	where 	
-		contr (y,xb) = y * log (cdf xb) + (1 - y) * log (1 - cdf xb)
-		cdf = cumulative (normalDistr 0 1) 
-
-dllprobit :: GradientF 
-dllprobit x y b = fmap negate $ U.toList $ transpose x `multiplyV` G.map contr (G.zip y (x `multiplyV` U.fromList b))
-	where 	
-		contr (y,xb) = y * pdf xb / cdf xb - (1 - y) * pdf xb / (1 - cdf xb)
+		loglik b = negate $ G.sum $ G.map contr (G.zip y (x `multiplyV` U.fromList b))
+		grad b = fmap negate $ U.toList $ transpose x `multiplyV` G.map gradcontr (G.zip y (x `multiplyV` U.fromList b))
+		contr (y,xb) = y * log (cdf xb) + (1.0 - y) * log (1.0 - cdf xb)
+		gradcontr (y,xb) = y * pdf xb / cdf xb - (1 - y) * pdf xb / (1 - cdf xb)
 		pdf = density (normalDistr 0 1) 
-		cdf = cumulative (normalDistr 0 1) 
+		cdf x = min (max (cumulative (normalDistr 0 1) x) (epsilon defaultMaxCtrl)) (1 - epsilon defaultMaxCtrl) -- BUG in cumulative: range not [0,1]
 
 probit :: Matrix -> Vector -> Vector
-probit = maxLikelihood llprobit dllprobit defaultMaxCtrl
+probit = maxLikelihood llprobit defaultMaxCtrl
--- examples/mle/probit.hs
+++ examples/mle/probit.hs
@@ -8,18 +8,17 @@ import Control.Monad
 import qualified Data.Vector.Generic as G
 import qualified Data.Vector.Unboxed as U
 
-main :: IO String 
+main :: IO () 
 main = do 
-	let n = 100 :: Int
-	let k = 5 :: Int
-	let b0 = 1.0
+	let n = 1000 :: Int
+	let k = 3 :: Int
+	let b0 = 0.0
 	let b1 = U.fromList $ take k (enumFrom 1.0)
+	let ratio b = G.map (\bk -> bk / (b G.! 0)) b
 
 	gen <- create
 	e <- fromList n 1 <$> replicateM n (genContinous (normalDistr 0 1) gen)
 	x <- fromList n k <$> replicateM (n * k) (genContinous (normalDistr 0 1) gen)
 	let y = G.zipWith3 (\x y z -> if x+y+z > 0 then 1.0 else 0.0 :: Double ) (G.replicate n b0) (x `multiplyV` b1) (toVector e)
-	let bhat2 = U.toList $ probit x y
-	putStrLn "Calculating probit..."
-	return (show bhat2) 
+	putStrLn $ "True values: " ++ show (ratio b1) ++ " Estimated values: " ++ show (ratio (probit x y))
 
