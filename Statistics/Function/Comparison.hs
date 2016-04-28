8378ca5b66721a27cf73b9878a0ecb4909d871b2
Alexey Khudyakov <alexey.skladnoy@gmail.com>
2016-04-28 22:13:15 +0300

    Deprecate  Statistics.Function.Comparison

--- Statistics/Function/Comparison.hs
+++ Statistics/Function/Comparison.hs
@@ -10,31 +10,9 @@
 -- Approximate floating point comparison, based on Bruce Dawson's
 -- \"Comparing floating point numbers\":
 -- <http://www.cygnus-software.com/papers/comparingfloats/comparingfloats.htm>
-
 module Statistics.Function.Comparison
+    {-# DEPRECATED "Use Numeric.MathFunctions.Comparison from math-functions" #-}
     (
       within
     ) where
-
-import Control.Monad.ST (runST)
-import Data.Primitive.ByteArray (newByteArray, readByteArray, writeByteArray)
-import Data.Word (Word64)
-
--- | Compare two 'Double' values for approximate equality, using
--- Dawson's method.
---
--- The required accuracy is specified in ULPs (units of least
--- precision).  If the two numbers differ by the given number of ULPs
--- or less, this function returns @True@.
-within :: Int                   -- ^ Number of ULPs of accuracy desired.
-       -> Double -> Double -> Bool
-within ulps a b = runST $ do
-  buf <- newByteArray 8
-  ai0 <- writeByteArray buf 0 a >> readByteArray buf 0
-  bi0 <- writeByteArray buf 0 b >> readByteArray buf 0
-  let big  = 0x8000000000000000 :: Word64
-      ai | ai0 < 0   = big - ai0
-         | otherwise = ai0
-      bi | bi0 < 0   = big - bi0
-         | otherwise = bi0
-  return $ abs (ai - bi) <= fromIntegral ulps
+import Numeric.MathFunctions.Comparison (within)
