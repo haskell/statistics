cad5ee928c234a7ae8d91a3ff89be260a5c8a8ba
Alexey Khudyakov <alexey.skladnoy@gmail.com>
2016-05-16 19:36:59 +0300

    Add chi-squared test for continous data

--- Statistics/Test/ChiSquared.hs
+++ Statistics/Test/ChiSquared.hs
@@ -2,6 +2,7 @@
 -- | Pearson's chi squared test.
 module Statistics.Test.ChiSquared (
     chi2test
+  , chi2testCont
   , module Statistics.Test.Types
   ) where
 
@@ -9,7 +10,7 @@ import Prelude hiding (sum)
 
 import Statistics.Distribution
 import Statistics.Distribution.ChiSquared
-import Statistics.Function (square)
+import Statistics.Function        (square)
 import Statistics.Sample.Internal (sum)
 import Statistics.Test.Types
 import Statistics.Types
@@ -35,7 +36,7 @@ chi2test :: (G.Vector v (Int,Double), G.Vector v Double)
          -> Maybe (Test ChiSquared)
 chi2test ndf vec
   | ndf <  0  = error $ "Statistics.Test.ChiSquare.chi2test: negative NDF " ++ show ndf
-  | n > 0     = Just Test
+  | n   > 0   = Just Test
               { testSignificance = pValue $ complCumulative d chi2
               , testStatistics   = chi2
               , testDistribution = chiSquared ndf
@@ -50,3 +51,25 @@ chi2test ndf vec
     chi2test :: Int -> U.Vector (Int,Double) -> Maybe (Test ChiSquared) #-}
 {-# SPECIALIZE
     chi2test :: Int -> V.Vector (Int,Double) -> Maybe (Test ChiSquared) #-}
+
+
+-- | Chi squared test for data with normal errors. Data is supplied in
+--   form of pair (observation with error, and expectation).
+chi2testCont
+  :: (G.Vector v (Estimate NormalErr Double, Double), G.Vector v Double)
+  => Int                                   -- ^ Number of additional
+                                           --   degrees of freedom.
+  -> v (Estimate NormalErr Double, Double) -- ^ Observation and expectation.
+  -> Maybe (Test ChiSquared)
+chi2testCont ndf vec
+  | ndf < 0   = error $ "Statistics.Test.ChiSquare.chi2testCont: negative NDF " ++ show ndf
+  | n   > 0   = Just Test
+              { testSignificance = pValue $ complCumulative d chi2
+              , testStatistics   = chi2
+              , testDistribution = chiSquared ndf
+              }
+  | otherwise = Nothing
+  where
+    n     = G.length vec - ndf - 1
+    chi2  = sum $ G.map (\(Estimate o (NormalErr s),e) -> square (o - e) / s) vec
+    d     = chiSquared n
