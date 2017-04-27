8f8e7e60a768ed2ae1ac6f19961b0d29dd846fc2
Alexey Khudyakov <alexey.skladnoy@gmail.com>
2017-04-27 18:15:29 +0300

    Fix serialization for DiscreteUniform
    Also style

--- Statistics/Distribution/DiscreteUniform.hs
+++ Statistics/Distribution/DiscreteUniform.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE DeriveDataTypeable, DeriveGeneric #-}
+{-# LANGUAGE DeriveDataTypeable, DeriveGeneric, OverloadedStrings #-}
 -- |
 -- Module    : Statistics.Distribution.DiscreteUniform
 -- Copyright : (c) 2016 André Szabolcs Szelp
@@ -28,53 +28,65 @@ module Statistics.Distribution.DiscreteUniform
     , rangeTo
     ) where
 
-import Data.Aeson (FromJSON, ToJSON)
-import Data.Binary (Binary)
-import Data.Data (Data, Typeable)
+import Control.Applicative ((<$>), (<*>), empty)
+import Data.Aeson   (FromJSON(..), ToJSON, Value(..), (.:))
+import Data.Binary  (Binary(..))
+import Data.Data    (Data, Typeable)
 import GHC.Generics (Generic)
+
 import qualified Statistics.Distribution as D
-import Data.Binary (put, get)
-import Control.Applicative ((<$>), (<*>))
+import Statistics.Internal
+
 
 
 -- | The discrete uniform distribution.
 data DiscreteUniform = U {
       rangeFrom  :: {-# UNPACK #-} !Int
-    -- ^ /a/, the lower bound of the support {a, ..., b} 
+    -- ^ /a/, the lower bound of the support {a, ..., b}
     , rangeTo    :: {-# UNPACK #-} !Int
-    -- ^ /b/, the upper bound of the support {a, ..., b} 
-    } deriving (Eq, Read, Show, Typeable, Data, Generic)
+    -- ^ /b/, the upper bound of the support {a, ..., b}
+    } deriving (Eq, Typeable, Data, Generic)
 
-instance FromJSON DiscreteUniform
-instance ToJSON DiscreteUniform
+instance Show DiscreteUniform where
+  showsPrec i (U a b) = defaultShow2 "discreteUniformAB" a b i
+instance Read DiscreteUniform where
+  readPrec = defaultReadPrecM2 "discreteUniformAB" (\a b -> Just (discreteUniformAB a b))
+
+instance ToJSON   DiscreteUniform
+instance FromJSON DiscreteUniform where
+  parseJSON (Object v) = do
+    a <- v .: "uniformA"
+    b <- v .: "uniformB"
+    return $ discreteUniformAB a b
+  parseJSON _ = empty
 
 instance Binary DiscreteUniform where
-    put (U a b) = put a >> put b
-    get = U <$> get <*> get
+  put (U a b) = put a >> put b
+  get         = discreteUniformAB <$> get <*> get
 
 instance D.Distribution DiscreteUniform where
-    cumulative (U a b) x
-        | x < fromIntegral a = 0
-        | x > fromIntegral b = 1
-        | otherwise = fromIntegral ((floor x) - a + 1) / fromIntegral (b - a + 1)
+  cumulative (U a b) x
+    | x < fromIntegral a = 0
+    | x > fromIntegral b = 1
+    | otherwise = fromIntegral (floor x - a + 1) / fromIntegral (b - a + 1)
 
 instance D.DiscreteDistr DiscreteUniform where
-    probability (U a b) k
-        | k >= a && k <= b = 1/fromIntegral (b - a + 1)
-        | otherwise = 0
+  probability (U a b) k
+    | k >= a && k <= b = 1 / fromIntegral (b - a + 1)
+    | otherwise        = 0
 
 instance D.Mean DiscreteUniform where
-    mean (U a b) = fromIntegral (a+b)/2
+  mean (U a b) = fromIntegral (a+b)/2
 
 instance D.Variance DiscreteUniform where
-    variance (U a b) = (fromIntegral (b - a + 1)^2 - 1) / 12
+  variance (U a b) = (fromIntegral (b - a + 1)^(2::Int) - 1) / 12
 
 instance D.MaybeMean DiscreteUniform where
-    maybeMean = Just . D.mean
+  maybeMean = Just . D.mean
 
 instance D.MaybeVariance DiscreteUniform where
-    maybeStdDev   = Just . D.stdDev
-    maybeVariance = Just . D.variance
+  maybeStdDev   = Just . D.stdDev
+  maybeVariance = Just . D.variance
 
 instance D.Entropy DiscreteUniform where
   entropy (U a b) = log $ fromIntegral $ b - a + 1
@@ -87,16 +99,14 @@ instance D.MaybeEntropy DiscreteUniform where
 discreteUniform :: Int             -- ^ Range
                 -> DiscreteUniform
 discreteUniform n
-    | n < 1     = error $ msg ++ "range must be > 0. Got " ++ show n
-    | otherwise = U 1 n
-        where msg = "Statistics.Distribution.DiscreteUniform.discreteUniform: "
+  | n < 1     = error $ msg ++ "range must be > 0. Got " ++ show n
+  | otherwise = U 1 n
+  where msg = "Statistics.Distribution.DiscreteUniform.discreteUniform: "
 
 -- | Construct discrete uniform distribution on support {a, ..., b}.
 discreteUniformAB :: Int             -- ^ Lower boundary (inclusive)
                   -> Int             -- ^ Upper boundary (inclusive)
                   -> DiscreteUniform
 discreteUniformAB a b
-    | b < a     = discreteUniformAB b a
-    | otherwise = U a b
-        where msg = "Statistics.Distribution.DiscreteUniform.discreteUniformAB: "
-
+  | b < a     = U b a
+  | otherwise = U a b
