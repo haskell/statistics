d1408ce7e402e38a8cce9fda89ce80752dc852d4
Lorenz Minder <lminder@gmx.net>
2022-12-22 16:44:55 -0600

    Geometric: Fix the tests.
    Also exclude the choice p = 0 in the QC.Arbitrary instances.

--- tests/Tests/Orphanage.hs
+++ tests/Tests/Orphanage.hs
@@ -44,9 +44,9 @@ instance QC.Arbitrary GammaDistribution where
 instance QC.Arbitrary BetaDistribution where
   arbitrary = betaDistr <$> QC.choose (1e-3,10) <*> QC.choose (1e-3,10)
 instance QC.Arbitrary GeometricDistribution where
-  arbitrary = geometric <$> QC.choose (0,1)
+  arbitrary = geometric <$> QC.choose (1e-10,1)
 instance QC.Arbitrary GeometricDistribution0 where
-  arbitrary = geometric0 <$> QC.choose (0,1)
+  arbitrary = geometric0 <$> QC.choose (1e-10,1)
 instance QC.Arbitrary HypergeometricDistribution where
   arbitrary = do l <- QC.choose (1,20)
                  m <- QC.choose (0,l)
