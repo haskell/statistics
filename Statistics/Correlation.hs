0c16d2f53b1508c36be0fbd8807aed06e5954701
Alexey Khudyakov <alexey.skladnoy@gmail.com>
2025-01-21 20:28:47 +0300

    Add spearman2 variant

--- Statistics/Correlation.hs
+++ Statistics/Correlation.hs
@@ -10,6 +10,7 @@ module Statistics.Correlation
     , pearsonMatByRow
       -- * Spearman correlation
     , spearman
+    , spearman2
     , spearmanMatByRow
     ) where
 
@@ -51,7 +52,7 @@ pearsonMatByRow m
 -- Spearman
 ----------------------------------------------------------------
 
--- | compute Spearman correlation between two samples
+-- | Compute Spearman correlation between two samples
 spearman :: ( Ord a
             , Ord b
             , G.Vector v a
@@ -69,6 +70,27 @@ spearman xy
   where
     (x, y) = G.unzip xy
 {-# INLINE spearman #-}
+
+-- | Compute Spearman correlation between two samples. Samples must
+--   have same length.
+spearman2 :: ( Ord a
+            , Ord b
+            , G.Vector v a
+            , G.Vector v b
+            , G.Vector v Int
+            , G.Vector v (Int, a)
+            , G.Vector v (Int, b)
+            )
+         => v a
+         -> v b
+         -> Double
+spearman2 xs ys
+  | nx /= ny  = error "Statistics.Correlation.spearman2: samples must have same length"
+  | otherwise = pearson $ G.zip (rankUnsorted xs) (rankUnsorted ys)
+  where
+    nx = G.length xs
+    ny = G.length ys
+{-# INLINE spearman2 #-}
 
 -- | compute pairwise Spearman correlation between rows of a matrix
 spearmanMatByRow :: Matrix -> Matrix
