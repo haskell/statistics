139fc65903cd478e4ed5478eb190c44cad44fbe1
Alex Mason <github@me.axman6.com>
2025-08-09 16:11:04 +1000

    Unroll exponentation uo to 5 for inlining.

--- Statistics/Sample/Fold.hs
+++ Statistics/Sample/Fold.hs
@@ -139,7 +139,11 @@ import Prelude hiding ((^), sum)
 
 -- (^) operator from Prelude is just slow.
 (^) :: Double -> Int -> Double
-x ^ 1 = x
+!x ^ 1 = x
+x ^ 2 = x * x
+x ^ 3 = x * x * x
+x ^ 4 = (x * x) * (x * x)
+x ^ 5 = (x * x) * x * (x * x)
 -- x ^ n = x * (x ^ (n-1))
 x0 ^ n0 = go (n0-1) x0 where
     go 0 !acc = acc
