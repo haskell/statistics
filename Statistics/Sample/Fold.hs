f646698cc3199fa96b896e84b87afd248f6ec5f9
Alex Mason <github@me.axman6.com>
2025-08-09 16:10:11 +1000

    Update haddock for Fold

--- Statistics/Sample/Fold.hs
+++ Statistics/Sample/Fold.hs
@@ -1,8 +1,8 @@
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE BangPatterns #-}
 -- |
--- Module    : Statistics.Sample
--- Copyright : (c) 2008 Don Stewart, 2009 Bryan O'Sullivan
+-- Module    : Statistics.Sample.Fold
+-- Copyright : (c) 2008 Don Stewart, 2009 Bryan O'Sullivan, 2025 Alex Mason
 -- License   : BSD3
 --
 -- Maintainer  : bos@serpentine.com
@@ -10,14 +10,70 @@
 -- Portability : portable
 --
 -- Commonly used sample statistics, also known as descriptive
--- statistics.
+-- statistics. They are provided as 'Control.Foldl.Fold''s
+-- allowing them to be applied to any 'Foldable' type containing
+-- 'Double's. The functions in "Statistics.Sample" are all
+-- implemented in terms of these folds.
+--
+-- The benefit of poviding these as 'Fold's is that they can be combined
+-- compute multiple statistics with a single pass:
+--
+-- >>> -- Compute the mean divided by the variance
+-- >>> F.fold (liftA2 (/) FF.mean FF.fastVariance) [1,2,3,4,5,6,7,8]
+-- 0.8571428571428571
+--
+-- 'Fold's can be modified in several ways; use 'F.premap' to extract
+-- values from the input before being passed into the fold:
+--
+-- >>> :t F.premap fst mean
+-- F.Fold (Double, a) Double
+--
+--
+-- They can be have their result mapped using `fmap`:
+--
+-- >>> -- single pass standard deviation
+-- >>> :t sqrt <$> fastVariance
+-- F.Fold Double Double
+--
+-- (They also have a 'Profunctor' instance, so also have 'lmap', 'rmap', 'dimap'
+-- to contravariantly map the inputs and covariantly map output).
+--
+-- And they can be combined 'Applicative'ly to produce new single pass
+-- combined statistics (or even non-statistical 'Fold's).
+--
+-- >>> differenceOfMeans = liftA2 (-) (F.premap fst mean) (F.premap snd mean)
+-- >>> :t differenceOfMeans
+-- F.Fold (Double, Double) Double
+-- >>> F.fold differenceOfMeans [(10,1), (20,2), (30,3), (40,4)]
+-- 22.5
+--
+-- 'Fold's also have 'Num', 'Floating', 'Fractional', `Semigroup` and 'Monoid'
+-- instances, which allow them to be combined succinctly:
+--
+-- >>> myMean = sum / F.genericLength
+-- >>> F.fold myMean [1..10.0]
+-- 5.5
+--
+-- >>> myStdDev = sqrt fastVariance
+-- >>> F.fold myStdDev [1..10]
+-- 2.8722813232690143
+-- >>> differenceOfMeans = F.premap fst mean - F.premap snd mean
+--
+--
+-- === Performance
+-- Inlining can make a very large difference in performance when defining 'Fold's,
+-- which is why all definitions in this module are marked @INLINE@. If you define
+-- 'Fold's which you export in your own modules and libraries, you should mark
+-- them as @INLINE@ unless you have a good reason not to. Doing so allows their
+-- definitions to be inlined into the definitions of folding functions, allowing
+-- for significantly greater unboxing to occur.
 
 module Statistics.Sample.Fold
     (
     -- * Descriptive functions
-    range
-    , minimum'
+    minimum'
     , maximum'
+    , range
 
     -- * Statistics of location
     , expectation
@@ -62,10 +118,11 @@ module Statistics.Sample.Fold
     , biExpectation
     , kbnSum
     , kbnSum'
+    , kbn
+    , summation
+    , summationVia
     -- * References
     -- $references
-
-
     ) where
 
 import Statistics.Function (square)
@@ -118,28 +175,68 @@ yielding to boxed returns and heap checks.
 -}
 
 
--- | /O(n)/ Range. The difference between the largest and smallest
+-- | Range. The difference between the largest and smallest
 -- elements of a sample.
 range :: F.Fold Double Double
 range = maximum' - minimum'
 {-# INLINE range #-}
 
+-- | Maximum specialised to Double, returns -∞ without input.
 maximum' :: F.Fold Double Double
 maximum' = F.Fold max (-1/0 :: Double) id where
 {-# INLINE minimum' #-}
 
+-- | Minimum specialised to Double, returns ∞ without input.
 minimum' :: F.Fold Double Double
 minimum' = F.Fold min ( 1/0 :: Double) id where
 {-# INLINE maximum' #-}
 
-kbnSum' :: F.Fold Double KBNSum
-kbnSum' = F.Fold add zero id
-{-# INLINE kbnSum' #-}
-
+-- | Sum Doubles using Kahan-Babuška-Neumaier summation for increased accuracy. If you need to compute partial sums for combination later, use @kbnSum'@.
 kbnSum :: F.Fold Double Double
-kbnSum = fmap kbn kbnSum'
+kbnSum = summationVia kbn
 {-# INLINE kbnSum #-}
 
+-- | Sum Doubles using Kahan-Babuška-Neumaier summation
+--   for increased accuracy. The @KBNSum@ type contains
+--   the internal state of the sum, and as it is a monoid,
+--   can be use to combine different sums accurately.
+--
+-- === __Examples:__
+-- >>> xs = [1,2,pi,1e-9]
+-- >>> ys = [4,5,2*pi,1e-11]
+-- >>> F.fold kbnSum' xs <> F.fold kbnSum' ys
+-- KBNSum 21.424777961779377 2.580967484452971e-15
+--
+-- >>> kbn it
+-- 21.42477796177938
+-- >>> F.fold kbnSum' (xs ++ ys)
+-- KBNSum 21.42477796177938 (-9.7174619434753e-16)
+--
+-- >>> kbn it
+-- 21.42477796177938
+kbnSum' :: F.Fold Double KBNSum
+kbnSum' = summation
+{-# INLINE kbnSum' #-}
+
+-- | Sum doubles using any instance of the 'Summation' class.
+summation :: Summation a => F.Fold Double a
+summation = F.Fold add zero id
+{-# INLINE summation #-}
+
+-- | Sum doubles by using the finalisation function for types in the
+--  'Summation' class. 'kbnSum' is equivalent to @summationVia kbn@.
+--
+-- >>> F.fold (summationVia kbn) [1,2,pi,1e-9,1e-17]
+-- 6.141592654589793
+-- >>> F.fold (summationVia Numeric.Sum.kb2) [1,2,pi,1e-9,9e-15]
+-- 6.141592654589802
+summationVia :: Summation s => (s -> Double) -> F.Fold Double Double
+summationVia f = f <$> summation
+{-# INLINE summationVia #-}
+
+-- Counts the number of elementes using Word for the count.
+doubleLength :: F.Fold a Double
+doubleLength = F.Fold (\n _ -> n+1) (0::Word) fromIntegral
 
 -- | Compute expectation of function over for sample.
 expectation :: (a -> Double) -> F.Fold a Double
@@ -148,14 +245,10 @@ expectation f = F.premap f kbnSum / doubleLength
 
 -- | Arithmetic mean.  This uses Kahan-Babuška-Neumaier
 -- summation, so is more accurate than 'welfordMean' unless the input
--- values are very large. This function is not subject to stream
--- fusion.
+-- values are very large.
 mean :: F.Fold Double Double
 mean = kbnSum / doubleLength
 {-# INLINE mean #-}
-
-doubleLength :: F.Fold a Double
-doubleLength = F.Fold (\n _ -> n+1) (0::Int) fromIntegral
 
 
 -- | Arithmetic mean.  This uses Welford's algorithm to provide
@@ -184,8 +277,7 @@ meanWeighted = F.Fold go (V 0 0) fini
                 w' = w + xw
 {-# INLINE meanWeighted #-}
 
--- | Harmonic mean.  This algorithm performs a single pass over
--- the sample.
+-- | Harmonic mean.
 harmonicMean :: F.Fold Double Double
 harmonicMean = F.Fold go (T 0 0) fini
   where
@@ -195,20 +287,20 @@ harmonicMean = F.Fold go (T 0 0) fini
 
 -- | Geometric mean of a sample containing no negative values.
 geometricMean :: F.Fold Double Double
-geometricMean = fmap exp (expectation log)
+geometricMean = exp (expectation log)
 {-# INLINE geometricMean #-}
 
 
 -- | Compute the /k/th central moment of a sample.  The central moment
 -- is also known as the moment about the mean.
 --
--- This function performs two passes over the sample, so is not subject
--- to stream fusion.
+-- The mean must be provided which may require two passes over
+-- the input data.
 --
 -- For samples containing many values very close to the mean, this
 -- function is subject to inaccuracy due to catastrophic cancellation.
-centralMoment :: Int
-              -> Double -- ^ mean
+centralMoment :: Int    -- ^ /k/
+              -> Double -- ^ /mean/
               -> F.Fold Double Double
 centralMoment a m
     | a < 0  = error "Statistics.Sample.centralMoment: negative input"
@@ -217,18 +309,19 @@ centralMoment a m
     | otherwise = expectation go
   where
     go x = (x-m) ^ a
-    -- m    = mean xs
 {-# INLINE centralMoment #-}
 
 
 -- | Compute the /k/th and /j/th central moments of a sample.
 --
--- This function performs two passes over the sample, so is not subject
--- to stream fusion.
+-- The mean must already be known.
 --
 -- For samples containing many values very close to the mean, this
 -- function is subject to inaccuracy due to catastrophic cancellation.
-centralMoments :: Int -> Int -> Double  -> F.Fold Double (Double, Double)
+centralMoments :: Int    -- ^ /i/
+               -> Int    -- ^ /j/
+               -> Double -- ^ /mean/
+               -> F.Fold Double (Double, Double) -- ^ /i/th & /j/th central moments
 centralMoments a b m
     | a < 2 || b < 2 = liftA2 (,) (centralMoment a m) (centralMoment b m)
     | otherwise      = F.Fold go (T1 0 0 0) fini
@@ -240,24 +333,27 @@ centralMoments a b m
 
 
 -- | Compute the skewness of a sample. This is a measure of the
--- asymmetry of its distribution.
+-- asymmetry of its distribution. The mean must already be known.
 --
 -- A sample with negative skew is said to be /left-skewed/.  Most of
 -- its mass is on the right of the distribution, with the tail on the
 -- left.
 --
--- > skewness $ U.to [1,100,101,102,103]
+-- >>> xs = [1,100,101,102,103]
+-- >>> m  = F.fold mean xs
+-- >>> F.fold (skewness m) xs
+-- -1.4976814499182607
+--
 -- > ==> -1.497681449918257
 --
 -- A sample with positive skew is said to be /right-skewed/.
 --
--- > skewness $ U.to [1,2,3,4,100]
--- > ==> 1.4975367033335198
+-- >>> xs = [1,2,3,4,100]
+-- >>> m  = F.fold mean xs
+-- >>> F.fold (skewness m) xs
+-- 1.4975367033335198
 --
 -- A sample's skewness is not defined if its 'variance' is zero.
---
--- This function performs two passes over the sample, so is not subject
--- to stream fusion.
 --
 -- For samples containing many values very close to the mean, this
 -- function is subject to inaccuracy due to catastrophic cancellation.
@@ -269,13 +365,11 @@ skewness m = (\(c3, c2) -> c3 * c2 ** (-1.5)) <$> centralMoments 3 2 m
 -- | Compute the excess kurtosis of a sample.  This is a measure of
 -- the \"peakedness\" of its distribution.  A high kurtosis indicates
 -- that more of the sample's variance is due to infrequent severe
--- deviations, rather than more frequent modest deviations.
+-- deviations, rather than more frequent modest deviations. The mean
+-- must already be known.
 --
 -- A sample's excess kurtosis is not defined if its 'variance' is
 -- zero.
---
--- This function performs two passes over the sample, so is not subject
--- to stream fusion.
 --
 -- For samples containing many values very close to the mean, this
 -- function is subject to inaccuracy due to catastrophic cancellation.
@@ -295,8 +389,8 @@ kurtosis m = (\(c4, c2) -> c4 / (c2 * c2) - 3) <$> centralMoments 4 2 m
 -- al. for numerical robustness, but require two passes over the
 -- sample data as a result.
 --
--- Because of the need for two passes, these functions are /not/
--- subject to stream fusion.
+-- The mean must already be known, and may require two passes over
+-- the data.
 
 -- | Maximum likelihood estimate of a sample's variance.  Also known
 -- as the population variance, where the denominator is /n/.
@@ -305,11 +399,6 @@ variance m =
     liftA2 (\s n -> if n > 1 then s / n else 0)
            (robustSumVar m) doubleLength
 {-# INLINE variance #-}
-
-
-robustSumVar :: Double -> F.Fold Double Double
-robustSumVar m = F.premap (square . subtract m) kbnSum
-{-# INLINE robustSumVar #-}
 
 
 -- | Unbiased estimate of a sample's variance.  Also known as the
@@ -321,43 +410,25 @@ varianceUnbiased m =
 {-# INLINE varianceUnbiased #-}
 
 
-{-
--- | Calculate mean and maximum likelihood estimate of variance. This
--- function should be used if both mean and variance are required
--- since it will calculate mean only once.
--- meanVariance :: Double -> F.Fold Double (Double,Double)
--- meanVariance m
---   | n > 1     = (m, robustSumVar m samp / fromIntegral n)
---   | otherwise = (m, 0)
---     where
---       n = G.length samp
--- {-# SPECIALIZE meanVariance :: U.Vector Double -> (Double,Double) #-}
--- {-# SPECIALIZE meanVariance :: V.Vector Double -> (Double,Double) #-}
-
--- | Calculate mean and unbiased estimate of variance. This
--- function should be used if both mean and variance are required
--- since it will calculate mean only once.
--- meanVarianceUnb :: (G.Vector v Double) => v Double -> (Double,Double)
--- meanVarianceUnb samp
---   | n > 1     = (m, robustSumVar m samp / fromIntegral (n-1))
---   | otherwise = (m, 0)
---     where
---       n = G.length samp
---       m = mean samp
--- {-# SPECIALIZE meanVarianceUnb :: U.Vector Double -> (Double,Double) #-}
--- {-# SPECIALIZE meanVarianceUnb :: V.Vector Double -> (Double,Double) #-}
--}
+-- | The square of differences from the mean:
+--
+-- \(robustSumVar(xs) = \sum_{i=0}^n{(x_i-m)^2}\)
+--
+--'variance' is the result of this divided by the number of inputs.
+robustSumVar :: Double -> F.Fold Double Double
+robustSumVar m = F.premap (square . subtract m) kbnSum
+{-# INLINE robustSumVar #-}
 
 -- | Standard deviation.  This is simply the square root of the
 -- unbiased estimate of the variance.
 stdDev :: Double -> F.Fold Double Double
-stdDev m = fmap sqrt $ varianceUnbiased m
+stdDev m = sqrt (varianceUnbiased m)
 {-# INLINE stdDev #-}
 
 -- | Standard error of the mean. This is the standard deviation
 -- divided by the square root of the sample size.
 stdErrMean :: Double -> F.Fold Double Double
-stdErrMean m = stdDev m / fmap sqrt doubleLength
+stdErrMean m = stdDev m / sqrt doubleLength
 {-# INLINE stdErrMean #-}
 
 robustSumVarWeighted :: Double -> F.Fold (Double,Double) V
@@ -372,13 +443,13 @@ varianceWeighted :: Double -> F.Fold (Double,Double) Double
 varianceWeighted wm = fini <$> robustSumVarWeighted wm
     where
       fini (V s w) = s / w
+{-# INLINE varianceWeighted #-}
 
 -- $cancellation
 --
 -- The functions prefixed with the name @fast@ below perform a single
 -- pass over the sample data using Knuth's algorithm. They usually
--- work well, but see below for caveats. These functions are subject
--- to array fusion.
+-- work well, but see below for caveats.
 --
 -- /Note/: in cases where most sample data is close to the sample's
 -- mean, Knuth's algorithm gives inaccurate results due to
@@ -413,17 +484,17 @@ fastVarianceUnbiased = fmap fini fastVar
 -- | Standard deviation.  This is simply the square root of the
 -- maximum likelihood estimate of the variance.
 fastStdDev :: F.Fold Double Double
-fastStdDev = fmap sqrt fastVariance
+fastStdDev = sqrt fastVariance
 {-# INLINE fastStdDev #-}
 
--- Avoids computing the length twice
+-- | Compute two expectations at once, to avoid computing the length twice.
 biExpectation :: (a -> Double) -> (a -> Double) -> F.Fold a V
-biExpectation f s = fini <$> F.length <*> F.premap f kbnSum <*> F.premap s kbnSum
-    where fini n a b = let n' = fromIntegral n in V (a/n') (b/n')
+biExpectation f s = fini <$> doubleLength <*> F.premap f kbnSum <*> F.premap s kbnSum
+    where fini n a b =  V (a/n) (b/n)
 {-# INLINE biExpectation #-}
 
 -- | Covariance of sample of pairs. For empty sample it's set to
---   zero
+--   zero. The means `muX` and `muY` must already be known.
 covariance ::(Double, Double) -> F.Fold (Double,Double) Double
 covariance (muX, muY) =
     liftA2 (\n x -> if n > 0 then x else 0)
@@ -434,7 +505,7 @@ covariance (muX, muY) =
 
 -- | Correlation coefficient for sample of pairs. Also known as
 --   Pearson's correlation. For empty sample it's set to zero.
---  The means `muX` and `muY` must be known.
+--   The means `muX` and `muY` must already be known.
 correlation :: (Double, Double) -> F.Fold (Double,Double) Double
 correlation (muX, muY) =
     fini <$> F.length <*> covF <*> varsF
